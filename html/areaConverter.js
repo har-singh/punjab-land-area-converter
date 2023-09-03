3d4b357f1c853a4da0cc3d41f5bd39495bffb24b
har-singh <51124520+har-singh@users.noreply.github.com>
2023-09-03 16:49:58 +0100

    Update areaConverter.js

--- html/areaConverter.js
+++ html/areaConverter.js
@@ -8,34 +8,38 @@ const marlaInput = document.getElementById('marla');
 // Function to update all fields based on input
 function areaConverter(source, valNum) {
     valNum = parseFloat(valNum);
+    const marlaConversionFactor = 272.25;
+    const kanalConversionFactor = 20 * marlaConversionFactor;
+    const acreConversionFactor = 8 * kanalConversionFactor;
+
     if (source == "sqFeet") {
         sqMetersInput.value = (valNum * 0.092903).toFixed(2);
-        acresInput.value = (valNum / 272.25 / 20 / 8).toFixed(6);
-        kanalInput.value = (valNum / 272.25 / 20).toFixed(6);
-        marlaInput.value = (valNum / 272.25).toFixed(6);
+        acresInput.value = (valNum / acreConversionFactor).toFixed(6);
+        kanalInput.value = (valNum / kanalConversionFactor).toFixed(6);
+        marlaInput.value = (valNum / marlaConversionFactor).toFixed(6);
     }
     if (source == "sqMeters") {
         sqFeetInput.value = (valNum * 10.7639).toFixed(2);
-        acresInput.value = (valNum * 0.0002471054).toFixed(6);
-        kanalInput.value = (valNum * 0.001976843).toFixed(6);
-        marlaInput.value = (valNum * 0.012292).toFixed(6);
+        acresInput.value = (valNum / 4046.86).toFixed(6);
+        kanalInput.value = (valNum / kanalConversionFactor).toFixed(6);
+        marlaInput.value = (valNum / marlaConversionFactor).toFixed(6);
     }
     if (source == "acres") {
-        sqFeetInput.value = (valNum * 43560).toFixed(2);
+        sqFeetInput.value = (valNum * acreConversionFactor).toFixed(2);
         sqMetersInput.value = (valNum * 4046.86).toFixed(2);
-        kanalInput.value = (valNum * 8).toFixed(2);
-        marlaInput.value = (valNum * 160).toFixed(2);
+        kanalInput.value = (valNum / 8).toFixed(2);
+        marlaInput.value = (valNum * 20).toFixed(2);
     }
     if (source == "kanal") {
-        sqFeetInput.value = (valNum * 5445).toFixed(2);
+        sqFeetInput.value = (valNum * kanalConversionFactor).toFixed(2);
         sqMetersInput.value = (valNum * 505.857).toFixed(2);
-        acresInput.value = (valNum / 8).toFixed(6);
+        acresInput.value = (valNum / acreConversionFactor).toFixed(6);
         marlaInput.value = (valNum * 20).toFixed(2);
     }
     if (source == "marla") {
-        sqFeetInput.value = (valNum * 272.25).toFixed(2);
+        sqFeetInput.value = (valNum * marlaConversionFactor).toFixed(2);
         sqMetersInput.value = (valNum * 50.5857).toFixed(2);
-        acresInput.value = (valNum / 160).toFixed(6);
+        acresInput.value = (valNum / kanalConversionFactor).toFixed(6);
         kanalInput.value = (valNum / 20).toFixed(2);
     }
 }
