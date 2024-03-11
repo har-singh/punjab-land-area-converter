67a80be9b7571cd4fbc7f946475816a75da31ee0
har-singh <harsingh@hotmail.com>
2024-03-11 12:14:44 +0000

    sarsai to areaConverter.js

--- html/areaConverter.js
+++ html/areaConverter.js
@@ -4,9 +4,11 @@ const sqMetersInput = document.getElementById('sqMeters');
 const acresInput = document.getElementById('acres');
 const kanalInput = document.getElementById('kanal');
 const marlaInput = document.getElementById('marla');
+const sarsaiInput = document.getElementById('sarsai');
 
 // Conversion constants
-const marlaSqft = 272.25;
+const sarsaiSqft = 30.25;
+const sarsaiMarla = 9; // 1 marla = 9 sarsai
 const kanalMarla = 20; // 1 kanal = 20 marla
 const acreKanal = 8; // 1 acre = 8 kanal
 // 1 m² = 10.7639104 ft²
@@ -27,6 +29,7 @@ function areaConverter(source, valNum) {
         acresInput.value = (valNum / 4046.86).toFixed(6);
         kanalInput.value = (valNum * acreKanal / 4046.86).toFixed(6);
         marlaInput.value = (valNum * kanalMarla * acreKanal / 4046.86).toFixed(2);
+        sarsaiInput.value = (valNum / sarsaiSqft).toFixed(6);
     }
     if (source == "acres") {
         sqFeetInput.value = (valNum * acreKanal * kanalMarla * marlaSqft).toFixed(2);
@@ -45,5 +48,13 @@ function areaConverter(source, valNum) {
         sqMetersInput.value = (valNum / kanalMarla / acreKanal * 4046.86).toFixed(2);
         acresInput.value = (valNum / kanalMarla / acreKanal).toFixed(6);
         kanalInput.value = (valNum / kanalMarla).toFixed(6);
+    }
+    if (source == "sarsai") {
+        sqFeetInput.value = (valNum * sarsaiSqft).toFixed(2);
+        sqMetersInput.value = (valNum * sarsaiSqft / 10.7639104).toFixed(2);
+        acresInput.value = (valNum * sarsaiMarla * kanalMarla * acreKanal / 9 / 10.7639104).toFixed(6);
+        kanalInput.value = (valNum * sarsaiMarla * kanalMarla / 9 / 10.7639104).toFixed(6);
+        marlaInput.value = (valNum * sarsaiMarla / 9).toFixed(6);
+        sarsaiInput.value = (valNum * sarsaiMarla).toFixed(2);
     }
 }
